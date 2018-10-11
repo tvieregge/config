80242a14901827ba3e2a5aeb486c3af2e7355c55
tvieregge <tmvieregge@gmail.com>
2018-10-11 15:53:32 -0700

    basic install script written, (very) untested

--- install.sh
+++ install.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
+#
+# If this gets any more complicated it's going to need to be an atomic
+# installation probably written in a more powerful language (python)
+# or start using the packag manager
+
+# install all packages in list
+# The list was generated with pacman -Qqe
+yaourt -S --needed - < pkglist.txt
+
+cwd=$(pwd)
+ln -fs "$cwd/.zshrc" ~/.zshrc
+ln -fs "$cwd/init.vim" ~/.config/nvim/init.vim
+ln -fs "$cwd/config" ~/config
+ln -fs "$cwd/move-cursor-window-center.sh" ~/move-cursor-window-center.sh
 
