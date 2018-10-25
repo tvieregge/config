6cbd971bd4866d9792e9df99785ce74a6aba38c1
tvieregge <tmvieregge@gmail.com>
2018-10-25 13:44:05 -0400

    installation fixed

--- .zshrc
+++ .zshrc
@@ -2,7 +2,7 @@
 # export PATH=$HOME/bin:/usr/local/bin:$PATH
 
 # Path to your oh-my-zsh installation.
-  export ZSH="/home/tim/.oh-my-zsh"
+  export ZSH="$HOME/.oh-my-zsh"
 
 
 # Set list of themes to pick from when loading at random
--- install.sh
+++ install.sh
@@ -5,20 +5,20 @@
 # or start using the packag manager
 
 # install all packages in list
-# The list was generated with pacman -Qqe
+# The list was generated with pacman -Qqet
 yaourt -S --needed - < pkglist.txt
 
 cwd=$(pwd)
 ln -fs "$cwd/.zshrc" ~/.zshrc
 ln -fs "$cwd/init.vim" ~/.config/nvim/init.vim
-ln -fs "$cwd/config" ~/config
+ln -fs "$cwd/config" ~/.i3/config
 ln -fs "$cwd/move-cursor-window-center.sh" ~/move-cursor-window-center.sh
 mkdir ~/.config/termite
 ln -fs "$cwd/termite-config" ~/.config/termite/config
 
 # Install service for locking screen when computer sleeps
-ln -fs "$cwd/lock" /usr/bin/lock
-ln -fs "$cwd/sleeplock.service" /etc/systemd/system/sleeplock.service
+sudo ln -fs "$cwd/lock" /usr/bin/lock
+sudo ln -fs "$cwd/sleeplock.service" /etc/systemd/system/sleeplock.service
 
 # make zsh the default shell
 chsh -s $(which zsh)
