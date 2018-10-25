0ff6e4f84028ed79e07226482cba4c8849f57412
tvieregge <tmvieregge@gmail.com>
2018-10-25 14:17:42 -0400

    adding sudo to pacman install

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@
 # The list was generated with pacman -Qqet
 #
 # TODO TEMPORARY: This makes it work, but filters out packages from the AUR
-pacman -S --needed $(comm -12 <(pacman -Slq | sort) <(sort pkglist.txt))
+sudo pacman -S --needed $(comm -12 <(pacman -Slq | sort) <(sort pkglist.txt))
 
 #install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
