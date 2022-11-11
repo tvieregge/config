50d1eb9dd3fa20d211a12e20df9a75bbedd519ef
Tim Vieregge <tmvieregge@gmail.com>
2022-11-11 15:44:32 -0500

    adding comments to mons display switching script about primary monitor

--- mons-single-display.sh
+++ mons-single-display.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+#
+# This script is based off of "secondary" and "primary" display This isn't very
+# realiable, but is very general. It gets messed up if you select the external
+# monitor as primary, but should work with any laptop.
+#
+# An alternative would be to `mons -S` and specify the display number
 
 case ${MONS_NUMBER} in
     1)
